caa2aa03068f5b456e9bdb4ed94f5e62c800add0
Gadzhi Kharkharov <1460122+kkga@users.noreply.github.com>
2021-03-07 10:54:39 +0300

    upd

--- App.svelte
+++ App.svelte
@@ -2,15 +2,15 @@
 	import Button from "./Button.svelte";
 </script>
 
+<main>
+	<h1>Hello CodeSandbox</h1>
+	<h2>Start editing to see some magic happen!</h2>
+	<Button />
+</main>
+
 <style>
   main {
     font-family: sans-serif;
     text-align: center;
   }
 </style>
-
-<main>
-	<h1>Hello CodeSandbox</h1>
-	<h2>Start editing to see some magic happen!</h2>
-	<Button />
-</main>
